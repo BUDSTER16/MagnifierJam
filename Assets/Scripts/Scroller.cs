c9d2d702a8fd25e374016b362d8ee98b7a04fc5f
Clare <claregpbenn@icloud.com>
2023-10-24 13:22:29 -0400

    RAwr XD

--- Assets/Scripts/Scroller.cs
+++ Assets/Scripts/Scroller.cs
@@ -10,9 +10,13 @@ public class Scroller : MonoBehaviour
     [SerializeField] private RawImage _img;
     [SerializeField] private float x, y;
 
+    public Transform player;
+
     // Update is called once per frame
     void Update()
     {
         _img.uvRect = new Rect(_img.uvRect.position + new Vector2(x, y) * Time.deltaTime, _img.uvRect.size);
+
+
     }
 }

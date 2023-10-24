0ada42477178719aaec00e8dced22243d495e74a
Clare <claregpbenn@icloud.com>
2023-10-24 13:38:42 -0400

    Somewhat Functional Background...

--- Assets/Scripts/Scroller.cs
+++ Assets/Scripts/Scroller.cs
@@ -10,13 +10,13 @@ public class Scroller : MonoBehaviour
     [SerializeField] private RawImage _img;
     [SerializeField] private float x, y;
 
-    public Transform player;
+    public Transform camera;
 
     // Update is called once per frame
     void Update()
     {
         _img.uvRect = new Rect(_img.uvRect.position + new Vector2(x, y) * Time.deltaTime, _img.uvRect.size);
-
+        transform.position = camera.transform.position + new Vector3(0, 0, 5);
 
     }
 }
